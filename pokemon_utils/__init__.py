c6240ddaea4a745f45e541290593cfcadc69d5c2
Kyra Paraiso <24mashpotato@gmail.com>
2025-09-04 07:45:34 +0800

    fixed init

---
+++ pokemon_utils/__init__.py
@@ -0,0 +1,3 @@
+from .pokemon_gif.get_pokemon_gif import get_pokemon_gif
+
+__all__ = ["get_pokemon_gif"]
--- pokemon_utils/pokemon_gif/__init__.py
+++
@@ -1,3 +0,0 @@
-from .get_pokemon_gif import get_pokemon_gif
-
-__all__ = ["get_pokemon_gif"]

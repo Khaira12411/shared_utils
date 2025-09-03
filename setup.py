196515ec0a782ddd6c499a932827a1d586d95cd7
Kyra Paraiso <24mashpotato@gmail.com>
2025-09-04 07:58:53 +0800

    commit

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="shared_utils",  # name of your package
-    version="0.1.0",  # start at 0.1.0
-    packages=find_packages(),  # automatically find packages in the folder
-    install_requires=[],  # add dependencies if your code needs any
-    python_requires=">=3.11",  # match your bots' Python version
+    name="shared_utils",
+    version="0.1.0",
+    packages=find_packages(include=["pokemon_utils", "pokemon_utils.*"]),
+    python_requires=">=3.11",
     description="Shared utilities for multiple bots",
     author="Khy",
-    url="https://github.com/Khaira12411/shared_utils",  # repo URL
+    url="https://github.com/Khaira12411/shared_utils",
 )
